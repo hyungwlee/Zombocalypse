0a05ce9ef6f287236516ae6e7cabc2b55bc18d73
DanteB98 <74515280+DanteB98@users.noreply.github.com>
2024-10-24 13:16:10 -0400

    Initial Commit

---
+++ Zombocalypse/ZombocalypseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZombocalypseApp.swift
+//  Zombocalypse
+//
+//  Created by Dante Betancourt on 10/24/24.
+//
+
+import SwiftUI
+
+@main
+struct ZombocalypseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ZombocalypseTests/ZombocalypseTests.swift
@@ -0,0 +1,17 @@
+//
+//  ZombocalypseTests.swift
+//  ZombocalypseTests
+//
+//  Created by Dante Betancourt on 10/24/24.
+//
+
+import Testing
+@testable import Zombocalypse
+
+struct ZombocalypseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
