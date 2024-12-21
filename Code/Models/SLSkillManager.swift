177eb08cf908e0c0d2d4bb9a844411673b9fdc87
DanteB98 <74515280+DanteB98@users.noreply.github.com>
2024-12-20 23:59:58 -0500

    Updated magnet and boss HP

--- Code/Models/SLSkillManager.swift
+++ Code/Models/SLSkillManager.swift
@@ -384,10 +384,10 @@ extension SLSkillManager {
                 type: .magnet,
                 maxLevel: 4,
                 levelEffects: [
-                    SLSkillLevelEffect(coinRadiusIncrement: 40),
-                    SLSkillLevelEffect(coinRadiusIncrement: 50),
-                    SLSkillLevelEffect(coinRadiusIncrement: 70),
-                    SLSkillLevelEffect(coinRadiusIncrement: 80)
+                    SLSkillLevelEffect(coinRadiusIncrement: 5),
+                    SLSkillLevelEffect(coinRadiusIncrement: 15),
+                    SLSkillLevelEffect(coinRadiusIncrement: 20),
+                    SLSkillLevelEffect(coinRadiusIncrement: 30)
                 ]
             )
         )
